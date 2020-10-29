67fa31e33c429c00e0477bbf436344b3fdf1d7f4
张乐 <892770342@qq.com>
2020-10-30 03:48:03 +0800

    leetcodelearning

---
+++ 二叉树/129. 求根到叶子节点数字之和.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        def dfs( root: TreeNode,Path=[]) -> int:
+            if not root:
+                return 0
+
+            if not root.left and not root.right:
+                return  sum([num*10**(i) for i,num in enumerate(reversed(Path))])
+
+            Path.append(root.left.val)
+            a=dfs(root.left,Path)
+            Path.pop()
+            Path.append(root.right.val)
+            b=dfs(root.right,Path)
+            Path.pop()
+            return a+b
+        path=[]
+        path.append(root.val)
+        return  dfs(root,path)
+
+if __name__ == '__main__':
+    print(Solution().sumNumbers())

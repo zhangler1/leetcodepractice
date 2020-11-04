c5c26f82b19da62662696f055772eb93db58a6b9
zhangle <892770342@qq.com>
2020-11-04 19:37:45 +0800

    704. 二分查找.py

---
+++ 二分查找/704. 二分查找.py
@@ -0,0 +1,28 @@
+from typing import  List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 输入: nums = [-1, 0, 3, 5, 9, 12], target = 9
+        # 输出: 4
+        # 解释: 9
+        # 出现在
+        # nums
+        # 中并且下标为
+        # 4
+        left=0
+        right=len(nums)-1
+        mid=(left+right)//2
+        pre=None
+        while(pre!=mid):
+            pre=mid
+            if nums[mid]>target:
+                right=mid-1
+                mid=(left+right)//2
+            elif nums[mid]<target:
+                left=mid+1
+                mid=(left+right)//2
+            else:
+                return mid
+        return -1
+if __name__ == '__main__':
+    print(Solution().search([-1,0,3,5,9,12], 9))
+

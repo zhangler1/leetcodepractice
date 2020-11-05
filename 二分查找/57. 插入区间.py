85ba5f293eb4c34b2c954e32dd68f47299baaab5
张乐 <892770342@qq.com>
2020-11-05 13:46:50 +0800

    #给之前的写的程序写了一个别名 #要记住呀！！ 名字里面带点号的，包引入的时候回变得麻烦。

---
+++ 二分查找/57. 插入区间.py
@@ -0,0 +1,8 @@
+from  typing import  List
+
+
+from 二分查找.binarySearch import Solution as Search#给之前的写的程序写了一个别名
+#要记住呀！！ 名字里面带点号的，包引入的时候回变得麻烦。
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        Search().search()

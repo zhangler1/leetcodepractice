f01cd9fc830a28f92b574f99dc8f8ab34b327795
张乐 <892770342@qq.com>
2020-11-19 15:52:24 +0800

    426.将二叉搜索树转化为排序的双向链表.py

--- array/283.移动零.py
+++ array/283.移动零.py
@@ -4,6 +4,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        n=len(nums)
+        zero=0
+        noZero=0
 
+        while zero<n and noZero<n:
+            noZero=zero
+            while zero<n and nums[zero]!=0:
+                zero+=1
+            while noZero <n and nums[noZero]==0:
+                noZero+=1
+            if zero!= n and noZero!=n:
+                if zero<noZero:
+                    nums[noZero],nums[zero]=nums[zero],nums[noZero]
+                # else:
+                #     break
+        return nums
 if __name__ == '__main__':
-    print(Solution().moveZeroes(        [0, 1, 0, 3, 12]))
+    print(Solution().moveZeroes([1,0,1]))
---
+++ 二叉树/426.将二叉搜索树转化为排序的双向链表.py
@@ -0,0 +1,41 @@
+from 二叉树.二叉树的序列化与反序列化297 import Codec
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def treeToDoublyList(self, root: "Node") -> "Node":
+        if not root:
+            return
+        pre:Node = None
+        head:Node=None
+
+        def treetopreandpost(root:Node):
+            if not root:
+                return
+            if root.left:  #其实没必要可能
+                treetopreandpost(root.left)
+            nonlocal pre
+            print(root.val)
+            if pre!=None:
+               root.left=pre
+
+               pre.right=root
+            else:
+              nonlocal head
+              head=root
+            # root
+            pre=root#用于保存顺序
+            treetopreandpost(root.right)
+
+
+        treetopreandpost(root)
+        pre.right=head
+        head.left=pre
+        return head
+if __name__ == '__main__':
+    print(Solution().treeToDoublyList(Codec().deserialize([])))

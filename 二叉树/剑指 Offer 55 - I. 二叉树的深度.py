6790445341427e8dfa6da8c7c68aaeaf043e20d6
张乐 <892770342@qq.com>
2020-11-20 22:24:53 +0800

    二叉树的序列化和反序列化.py 链表序列化.py

---
+++ 二叉树/剑指 Offer 55 - I. 二叉树的深度.py
@@ -0,0 +1,9 @@
+from 二叉树.二叉树的序列化和反序列化 import Codec,TreeNode
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
+
+if __name__ == '__main__':
+    print(Solution().maxDepth(Codec().deserialize([3,9,20,None,None,15,7])))
